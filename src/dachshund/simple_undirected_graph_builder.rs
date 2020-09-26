6538bb48ba89e72af44169e44cb6317586e2dcf0
bogdanstate <bogdanstate@gmail.com>
2020-09-26 02:48:24 -0700

    SimpleUndirectedGraph now uses SimpleNode

--- src/dachshund/simple_undirected_graph_builder.rs
+++ src/dachshund/simple_undirected_graph_builder.rs
@@ -5,63 +5,36 @@
  * LICENSE file in the root directory of this source tree.
  */
 extern crate nalgebra as na;
-use crate::dachshund::error::CLQResult;
-use crate::dachshund::graph_builder::GraphBuilder;
-use crate::dachshund::id_types::{EdgeTypeId, NodeId};
-use crate::dachshund::node::{Node, NodeEdge};
+use crate::dachshund::id_types::NodeId;
+use crate::dachshund::node::SimpleNode;
 use crate::dachshund::simple_undirected_graph::SimpleUndirectedGraph;
-use std::collections::{BTreeMap, HashMap, HashSet};
+use std::collections::{BTreeMap, BTreeSet, HashMap};
 
 use rand::prelude::*;
 
 pub struct SimpleUndirectedGraphBuilder {}
-impl GraphBuilder<SimpleUndirectedGraph> for SimpleUndirectedGraphBuilder {
-    fn _new(
-        nodes: HashMap<NodeId, Node>,
-        core_ids: Vec<NodeId>,
-        non_core_ids: Vec<NodeId>,
-    ) -> CLQResult<SimpleUndirectedGraph> {
-        assert!(core_ids.len() == non_core_ids.len());
-
-        Ok(SimpleUndirectedGraph {
-            nodes,
-            ids: core_ids,
-        })
-    }
-}
 
 impl SimpleUndirectedGraphBuilder {
     // builds a graph from a vector of IDs. Repeated edges are ignored.
     // Edges only need to be provided once (this being an undirected graph)
     #[allow(clippy::ptr_arg)]
     pub fn from_vector(data: &Vec<(i64, i64)>) -> SimpleUndirectedGraph {
-        let mut ids: BTreeMap<NodeId, HashSet<NodeId>> = BTreeMap::new();
+        let mut ids: BTreeMap<NodeId, BTreeSet<NodeId>> = BTreeMap::new();
         for (id1, id2) in data {
             ids.entry(NodeId::from(*id1))
-                .or_insert_with(HashSet::new)
+                .or_insert_with(BTreeSet::new)
                 .insert(NodeId::from(*id2));
             ids.entry(NodeId::from(*id2))
-                .or_insert_with(HashSet::new)
+                .or_insert_with(BTreeSet::new)
                 .insert(NodeId::from(*id1));
         }
-        let edge_type_id = EdgeTypeId::from(0 as usize);
-        let mut nodes: HashMap<NodeId, Node> = HashMap::new();
+        let mut nodes: HashMap<NodeId, SimpleNode> = HashMap::new();
         for (id, neighbors) in ids.into_iter() {
             nodes.insert(
                 id,
-                Node {
+                SimpleNode {
                     node_id: id,
-                    edges: neighbors
-                        .iter()
-                        .map(|x| NodeEdge::new(edge_type_id, *x))
-                        .collect(),
                     neighbors: neighbors
-                        .iter()
-                        .map(|x| (*x, vec![NodeEdge::new(edge_type_id, *x)]))
-                        .collect(),
-                    // meaningless
-                    is_core: true,
-                    non_core_type: None,
                 },
             );
         }
