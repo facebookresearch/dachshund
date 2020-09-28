1a93fef06d61ca9116e30a91e0389993b9adfc49
Bogdan State <bogdanstate@gmail.com>
2020-09-29 11:07:15 +1300

    Forgot to add GraphBuilderBase

---
+++ src/dachshund/graph_builder_base.rs
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+use crate::dachshund::graph_base::GraphBase;
+pub trait GraphBuilderBase
+where
+    Self: Sized,
+    Self::GraphType: GraphBase
+{
+    type GraphType;
+
+    fn from_vector(data: &Vec<(i64, i64)>) -> Self::GraphType;
+}
+
